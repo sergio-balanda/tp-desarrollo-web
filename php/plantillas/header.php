2bd56e33c274314eebe977ed927d0f36f293a7ce
unknown <NSIERRA@st14792.almafuerte.gov.ar>
2017-09-14 10:29:59 -0300

    arreglo bd - header.php se ve usuario logueado

--- php/plantillas/header.php
+++ php/plantillas/header.php
@@ -20,11 +20,11 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
-        <?php echo "<li><a href='#'> Bienvenido, ".$_SESSION['nombre']." </a></li>" ?>
+        <?php echo "<li><a href='#'> Bienvenido, ".$_SESSION['usuario']." </a></li>" ?>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Acceder <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Logout</a></li>
+            <li><a href="../php/login/salir.php">Logout</a></li>
             <li><a href="#">Front</a></li>
             <li><a href="#">Something else here</a></li>
             <li role="separator" class="divider"></li>
