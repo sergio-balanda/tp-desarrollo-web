7b087994fae691c291b71cd7e02a7c6af9dfdc59
sergio-balanda <sergio.f.balanda@gmail.com>
2017-09-20 18:53:42 -0300

    Delete login.php

--- php/login/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once '../conexion/conexion.php';
-    date_default_timezone_set('America/Argentina/Buenos_Aires');
-	
-	$user = $_POST['num_doc'];
-	$pass = md5($_POST['password']);
-	$hoy = date("Y-m-d H:i:s");
-    print_r($user);
-print_r($pass);
